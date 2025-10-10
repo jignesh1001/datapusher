1110fdfa6e3de4d8172faf6b3b9958e3e8d090b6
jignesh@1001 <jigneshgurav12@gmail.com>
2025-10-10 16:18:55 +0530

    log setup

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
 import authRoutes from "./routes/auth.routes.js";
 import accountRoutes from "./routes/account.routes.js";
+import destinationRoutes from "./routes/destination.routes.js";
+import dataHandlerRoutes from "./routes/dataHandler.routes.js";
+import logRoutes from "./routes/log.routes.js";
+import { swaggerSpec, swaggerUiSetup } from "./config/swagger.js";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 const app = express();
@@ -11,14 +16,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rate Limiting
+// rate Limiting
 const limiter = rateLimit({
   windowMs: 60 * 1000,
   max: 60,
+
 });
 app.use(limiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/accounts", accountRoutes);
+app.use("/api/destinations", destinationRoutes);
+app.use("/server", dataHandlerRoutes);
+app.use("/api/logs", logRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUiSetup.setup(swaggerSpec));
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { seedRoles } from "./config/seedRoles.js";
 
+
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(seedRoles);
+import "./workers/dataProcessor.worker.js";
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
