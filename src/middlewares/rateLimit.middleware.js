d11507ee734a8908436c797dc8ff9561fe20aa30
jignesh@1001 <jigneshgurav12@gmail.com>
2025-10-10 16:09:18 +0530

    testing rate limiter

--- src/middlewares/rateLimit.middleware.js
+++ src/middlewares/rateLimit.middleware.js
@@ -2,10 +2,16 @@ import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 
 export const createAccountLimiter = rateLimit({
   windowMs: 1000, // 1 second
-  max: 5,         // limit each account to 5 requests/second
+  max: 5, // limit each account to 5 requests/second
   message: {
     success: false,
-    message: "Too many requests, please try again later."
+    message: "Too many requests, please try again later.",
   },
-  keyGenerator: (req) => req.headers["cl-x-token"] || ipKeyGenerator(req)
+  keyGenerator: (req) => req.headers["cl-x-token"] || ipKeyGenerator(req),
+  handler: (req, res /*, next*/) => {
+    res.status(429).json({
+      success: false,
+      message: "Too many requests, please try again later.",
+    });
+  },
 });
